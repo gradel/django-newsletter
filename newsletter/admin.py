df7f35690f27e408216f7f06df9bd09cb30bf698
gradel <gerald@hien-online.de>
2017-08-29 18:19:23 +0200

    hide url and image fields of article

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -219,7 +219,7 @@ class ArticleInline(AdminImageMixin, StackedInline):
             'fields': ('title', 'text')
         }),
         (_('Optional'), {
-            'fields': ('sortorder', 'url', 'image'),
+            'fields': ('sortorder',),
             'classes': ('collapse',)
         }),
     )
