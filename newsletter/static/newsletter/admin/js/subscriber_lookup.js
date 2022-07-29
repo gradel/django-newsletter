4818ef98e9bfe05d8f1ed365c8cf58d49f431fed
gradel <gerald@hien-online.de>
2022-07-29 21:54:58 +0200

    Submission admin change form: fix subscriber lookup js

--- newsletter/static/newsletter/admin/js/subscriber_lookup.js
+++ newsletter/static/newsletter/admin/js/subscriber_lookup.js
@@ -1,7 +1,7 @@
 var JsonSubscribers = {
     init: function(inputname, add) {
         inp = document.getElementById(inputname);
-        addEvent(inp, "change", function(e) { JsonSubscribers.setSubscribers(inp.value); });
+        inp.addEventListener("change", function(e) { JsonSubscribers.setSubscribers(inp.value); });
         if (add==true && inp.value != "") {
             JsonSubscribers.setSubscribers(inp.value);
         }
@@ -11,24 +11,20 @@ var JsonSubscribers = {
         SelectBox.move_all('id_subscriptions_to', 'id_subscriptions_from');
 
         if (id) {
-            xmlhttp.open( "GET", "/admin/newsletter/message/"+id+"/subscribers/json/", true );
-            xmlhttp.onreadystatechange=function() {
-                if (xmlhttp.readyState==4 && xmlhttp.status == 200) {
-
-                    objects = eval( "(" + xmlhttp.responseText + ")" );
-
-                    var from_box = document.getElementById('id_subscriptions_from');
-                    for (var i = 0; (option = from_box.options[i]); i++) {
-                        for (j=0;(object = objects[j]);j++) {
-                            if (object.pk.toString() == option.value) {
-                                option.selected = true;
-                            }
+            fetch("/admin/newsletter/message/"+id+"/subscribers/json/")
+            .then((response) => response.text())
+            .then((data) => {
+                objects = eval( "(" + data + ")" );
+                var from_box = document.getElementById('id_subscriptions_from');
+                for (var i = 0; (option = from_box.options[i]); i++) {
+                    for (j=0;(object = objects[j]);j++) {
+                        if (object.pk.toString() == option.value) {
+                            option.selected = true;
                         }
                     }
-                    SelectBox.move('id_subscriptions_from', 'id_subscriptions_to');
                 }
-            }
-            xmlhttp.send(null)
+                SelectBox.move('id_subscriptions_from', 'id_subscriptions_to');
+            });
         }
     }
 };
